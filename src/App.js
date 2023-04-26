d1d865f21cce482b43af735bb6d2d282936ff515
Rushin-Nemz <rushinnemlawala@gmail.com>
2023-04-26 16:52:58 +0530

    My portfolio

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ function App() {
       <Router>
       <Navigation/>
         <Routes>
-          <Route exact path="/" element={<Home/>}/>
+          <Route exact path="/home" element={<Home/>}/>
           <Route exact path="/projects" element={<Projects/>}/>
           <Route exact path="/experience" element={<Experience/>}/>
         </Routes>
